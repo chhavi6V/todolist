f7da766bee01e3e9c80077978a51049b30dff97b
chhavi6V <chhavigupta597@gmail.com>
2023-06-07 15:01:18 +0530

    complete

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+
+console.log(date());
 
 const app = express();
 let items  = [];
@@ -11,15 +14,7 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.use(express.static("public")); 
 
 app.get("/",function(req,res){
-    let today = new Date();
-
-    let options = {
-        weekday : "long",
-        day : "numeric" ,
-        month :"long"
-    };
-
-    let day = today.toLocaleDateString("en-US",options);
+    let day = date(); 
 
     res.render("lists",{
         listTitle : day,
